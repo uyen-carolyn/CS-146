b0584979ca7c033361d949d6169528294da9e37d
Uyen Nguyen <uyen.nguyen@sjsu.edu>
2018-10-01 20:07:05 -0700

    Update InsertionSort.java

--- SortingAlgorithms/InsertionSort.java
+++ SortingAlgorithms/InsertionSort.java
@@ -1,3 +1,9 @@
+/**
+ * Insertion Sort based off Insertion Sort algorithm used in  
+ * "Introduction to Algorithms" (Third Edition) by by Thomas H. Cormen, 
+ * Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein
+ *
+ */
 public class InsertionSort {
 	public static int[] sort(int[] s) {
 		for (int i = 1; i < s.length; i++) {
@@ -25,6 +31,10 @@ public class InsertionSort {
 			System.out.print(s[i] + " ");
 	}
 
+	/**
+ 	 * Tests sorting algorithm on an array of 20 distinct number elements 
+	 * and returns the time it takes for best, average, and worst case scenario. 
+         */
 	public static void main(String args[]) {
 		int best[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 		int avg[] = { 3, 5, 7, 11, 2, 4, 6, 9, 16, 1, 8, 10, 20, 12, 15, 13, 14, 17, 19, 18 };
